b7b1db12fcc96da0d1677a6888e6968642b203ff
Nikunj Navdiya <9723282128nnm@gmail.com>
2024-02-10 15:03:05 +0530

    Added .env in react

--- frontend/src/pages/components/generateCertificate.jsx
+++ frontend/src/pages/components/generateCertificate.jsx
@@ -11,8 +11,7 @@ import { DateField } from "@mui/x-date-pickers/DateField";
 import Certificate, { downloadPDF } from "./Certificate";
 import Button from "../atoms/Button";
 import axios from "axios";
-const JWT =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5MjcwZTY3Ni1iZGVkLTRlN2EtYjgzMy04MmMwMTE1MDAyODciLCJlbWFpbCI6ImtyaXBuaWNrM0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZDlmODU2NDBjODI3MDc0ZTE3MDkiLCJzY29wZWRLZXlTZWNyZXQiOiI1YWM2ODkxYTI2MzhmZTNjZWY1ZGRlZDMwYzVlZDRiYmU0YzE4YjYxYTM5NDNkMmNhYWM2YjEzMzY0ZGQ5NDY3IiwiaWF0IjoxNzA2ODkxMjg3fQ.4mlbR8uKFxcsdtZFcqqCvt8arpg7UR5XDVDAeYQCw7E";
+const JWT = import.meta.env.VITE_IPFS_JWT;
 import Web3 from "web3";
 import CertiABI from "../../certificate.json";
 import LinearProgress from "@mui/material/LinearProgress";
@@ -55,7 +54,7 @@ export default function GenerateCertificate() {
   };
 
   const contractCall = async (hash) => {
-    const contractAddress = "0x23d6E35159Cc6979667577d50F1148f30bb8E01d";
+    const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
     try {
       await window.ethereum
         .request({ method: "eth_requestAccounts" })
@@ -80,9 +79,9 @@ export default function GenerateCertificate() {
                 transition: Slide,
               });
               var data = {
-                service_id: 'service_091mv0p',
-                template_id: 'template_63hkzbf',
-                user_id: 'szL2tKmkdmm1fZJ5W',
+                service_id: import.meta.env.VITE_SERVICE_ID,
+                template_id: import.meta.env.VITE_TEMPLATE_ID,
+                user_id: import.meta.env.VITE_USER_ID,
                 template_params: {
                   'to_name': formData.studentName,
                   'eventName': formData.eventName,
